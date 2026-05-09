a5a99d91ae3939289b9e8c39b6db72820223bfa1
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-09 15:56:01 +0530

    UC-3: Add part-time employee and daily wage calculation

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,17 +4,21 @@ class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int employeePresent = 1;
+        int partTimeEmployee = 2;
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 8;
 
         int employeeHours = 0;
         int employeeWage = 0;
 
         Random random = new Random();
-        int employeeCheck = random.nextInt(2);
+        int employeeCheck = random.nextInt(3);
         if (employeePresent == employeeCheck) {
             System.out.println("Employee is Present");
             employeeHours = fullDayHour;
+        } else if (partTimeEmployee == employeeCheck) {
+            employeeHours = partTimeHour;
         } else {
             System.out.println("Employee is Absent");
         }
