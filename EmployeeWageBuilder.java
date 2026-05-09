e481795df3decfba6db693e3edbefca245285b45
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-09 15:37:24 +0530

    UC-2: Daily employee wages is calculated.

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,10 +4,22 @@ class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int employeePresent = 1;
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+
+        int employeeHours = 0;
+        int employeeWage = 0;
 
         Random random = new Random();
         int employeeCheck = random.nextInt(2);
-        String status = employeePresent == employeeCheck ? "Employee is Present" : "Employee is Absent";
-        System.out.println(status);
+        if (employeePresent == employeeCheck) {
+            System.out.println("Employee is Present");
+            employeeHours = fullDayHour;
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+        employeeWage = employeeHours * wagePerHour;
+        System.out.println("Daily Employee Wages : " + employeeWage);
     }
 }
