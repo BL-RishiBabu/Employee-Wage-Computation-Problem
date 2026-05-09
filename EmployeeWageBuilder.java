8a974cc46dfb46860c26542c634630e2cea96405
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-09 15:21:58 +0530

    UC-1: Added welcome message and random attendance check

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,13 @@
+import java.util.Random;
+
 class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        int employeePresent = 1;
+
+        Random random = new Random();
+        int employeeCheck = random.nextInt(2);
+        String status = employeePresent == employeeCheck ? "Employee is Present" : "Employee is Absent";
+        System.out.println(status);
     }
 }
