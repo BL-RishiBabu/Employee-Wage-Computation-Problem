d5f1af3408ad988595037cbbae36e682a269ef00
Rishi <rishibabusoftwaredeveloper@gmail.com>
2026-05-12 10:55:29 +0530

    UC6: Calculate monthly wage till 100 working hours or 20 days is reached

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -9,9 +9,12 @@ class EmployeeWageBuilder {
         int fullDayHour = 8;
         int partTimeHour = 4;
         int dailyWage = 0;
-        int workingDaysPerMonth = 20;
         int totalEmployeeWage = 0;
         int employeeHours = 0;
+        int totalEmployeeHours = 0;
+        int totalWorkingDays = 0;
+        int maxWorkingDaysPerMonth = 20;
+        int maxWorkingHoursPerMonth = 100;
 
         Random random = new Random();
         int employeeCheck = random.nextInt(2) + 1;
@@ -22,7 +25,8 @@ class EmployeeWageBuilder {
             System.out.println("Employee is Part-Time");
         }
 
-        for (int day = 1; day <= workingDaysPerMonth; day++) {
+        while (totalEmployeeHours < maxWorkingHoursPerMonth && totalWorkingDays < maxWorkingDaysPerMonth) {
+            totalWorkingDays++;
             int employeePresents = random.nextInt(2);
 
             if (employeePresents == 1) {
@@ -30,7 +34,6 @@ class EmployeeWageBuilder {
                     case fullTimeEmployee -> {
                         employeeHours = fullDayHour;
                     }
-
                     case partTimeEmployee -> {
                         employeeHours = partTimeHour;
                     }
@@ -39,11 +42,19 @@ class EmployeeWageBuilder {
                 employeeHours = 0;
             }
 
+            if (totalEmployeeHours + employeeHours > maxWorkingHoursPerMonth) {
+                employeeHours = maxWorkingHoursPerMonth - totalEmployeeHours;
+            }
+
+            totalEmployeeHours += employeeHours;
             dailyWage = employeeHours * wagePerHour;
             totalEmployeeWage += dailyWage;
-            System.out.println("Day " + day + " Wage: " + dailyWage);
+
+            System.out.println("Day " + totalWorkingDays + " Hours: " + employeeHours + " | Daily Wage: " + dailyWage);
         }
 
-        System.out.println("\nDaily Employee Wages : " + totalEmployeeWage);
+        System.out.println("\nTotal Days Worked: " + totalWorkingDays);
+        System.out.println("Total Hours Worked: " + totalEmployeeHours);
+        System.out.println("Total Monthly Employee Wage: " + totalEmployeeWage);
     }
 }
